7cb849f7dcb6ac8a6418a20915ca9c1f7c7d82e5
johndev86 <johnlee@Johns-MacBook-Pro.local>
2019-02-22 23:38:56 -0500

    Updates

---
+++ precache-manifest.9127998a56161e8c35003eb0981ba5f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "ad84e391dd872ca5bd0d",
+    "url": "/robofriends/static/js/main.ad84e391.chunk.js"
+  },
+  {
+    "revision": "3ca4cea8691454c9b13f",
+    "url": "/robofriends/static/js/1.3ca4cea8.chunk.js"
+  },
+  {
+    "revision": "ad84e391dd872ca5bd0d",
+    "url": "/robofriends/static/css/main.66dde7c3.chunk.css"
+  },
+  {
+    "revision": "3ca4cea8691454c9b13f",
+    "url": "/robofriends/static/css/1.c43428ed.chunk.css"
+  },
+  {
+    "revision": "aecef1c4f4c407152440c39da8ed9ff7",
+    "url": "/robofriends/index.html"
+  }
+];
